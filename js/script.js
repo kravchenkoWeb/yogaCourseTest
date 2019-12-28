bbb72e576d34331bf3a4f5856d9ba2ef4ceb2537
user.name <kravchenko.web@gmail.com>
2019-12-28 22:26:26 +0300

    Added timer

--- js/script.js
+++ js/script.js
@@ -36,4 +36,45 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    //Timer
+
+    let deadLine = '2020-01-12';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds');
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+            
+        }  
+        
+        let timeInterval = setInterval(updateClock(), 1000);  
+    }
+
+    setClock('timer', deadLine);
 });
